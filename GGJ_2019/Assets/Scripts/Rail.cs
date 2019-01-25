cbe3f99aa88a6f33c228fa3850b80983bcbbf9f2
Etienne Champagne <etienne.champagne.ec@gmail.com>
2019-01-26 00:07:16 +0100

    Base Project

---
+++ GGJ_2019/Assets/Scripts/Rail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    [SerializeField] private Transform Bot;
+    [SerializeField] private Transform Top;
+    [SerializeField] private Transform Wall;
+
+    void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+        Wall.localPosition = new Vector3(0, 0, Wall.localPosition.z);
+    }
+
+    private void LateUpdate()
+    {
+        if (Wall.localPosition.z > Top.localPosition.z)
+        {
+            Wall.localPosition = new Vector3(0, 0, Top.localPosition.z);
+        }
+        else if (Wall.localPosition.z < Bot.localPosition.z)
+        {
+            Wall.localPosition = new Vector3(0, 0, Bot.localPosition.z);
+        }
+    }
+}
