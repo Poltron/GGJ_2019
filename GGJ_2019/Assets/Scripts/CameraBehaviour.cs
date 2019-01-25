42622db663de72e10abb1b0cfe7359088ca63576
Etienne Champagne <etienne.champagne.ec@gmail.com>
2019-01-25 21:55:04 +0100

    Base project

---
+++ GGJ_2019/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform ToFollow;
+
+	void Start ()
+    {
+	}
+	
+	void LateUpdate ()
+    {
+        transform.position = new Vector3(ToFollow.position.x, transform.position.y, ToFollow.position.z);
+	}
+}
---
+++ GGJ_2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+
+    private Vector2 velocity;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.Z))
+        {
+            velocity.y += 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y -= 1;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x += 1;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            velocity.x -= 1;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+
+        velocity.Normalize();
+
+        Vector3 movement = new Vector3(velocity.x, 0, velocity.y);
+
+        transform.position += movement * Speed * Time.deltaTime;
+	}
+}
