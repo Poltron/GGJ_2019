ee2b742e35652676fc9a154528f53f5eec40ddcf
Vincent YVERT <v.yvert@student.isartdigital.com>
2019-01-26 19:54:46 +0100

    LE SON DU DECES ITEMIEN

--- GGJ_2019/Assets/Scripts/Item.cs
+++ GGJ_2019/Assets/Scripts/Item.cs
@@ -6,13 +6,22 @@ public class Item : MonoBehaviour
 {
     [SerializeField] private PlayerController owner;
 
+    [SerializeField] private List<AudioClip> m_AudioClips;
+
+    private AudioSource m_AudioSource;
+
 	void Start ()
     {
+        m_AudioSource = gameObject.GetComponent<AudioSource>();
+
         owner.AddItem(this);
 	}
 	
     public void Destroyed()
     {
+        m_AudioSource.clip = m_AudioClips[Random.Range(0, m_AudioClips.Count)];
+        m_AudioSource.Play();
+
         owner.RemoveItem(this);
     }
 }
