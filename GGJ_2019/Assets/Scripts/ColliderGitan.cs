860b21504f26cc4cba7317050d73dafa6932db1b
Etienne Champagne <etienne.champagne.ec@gmail.com>
2019-01-26 13:31:56 +0100

    basic game loop

---
+++ GGJ_2019/Assets/Scripts/ColliderGitan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderGitan : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 10.0f * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        if (other.tag == "Item")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ GGJ_2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        Menu,
+        Playing,
+        EndGame
+    }
+
+    public GameState gameState;
+
+    private Coroutine nextMeteo;
+
+    public float timeToWait;
+    public float decrementTimeToWait;
+    public float minTimetoWait;
+
+    public WindSpawner[] windSpawners;
+
+    private void Start()
+    {
+        gameState = GameState.Menu;
+    }
+
+    void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gameState == GameState.Menu)
+                StartGame();
+            else if (gameState == GameState.Playing)
+                EndGame();
+        }
+	}
+
+    public void StartGame()
+    {
+        gameState = GameState.Playing;
+
+        StartCoroutine(timer(timeToWait, TriggerSpawner));
+    }
+
+    public void EndGame()
+    {
+        gameState = GameState.EndGame;
+    }
+
+    public void TriggerSpawner()
+    {
+        int rdm = UnityEngine.Random.Range(0, windSpawners.Length - 1);
+
+        windSpawners[rdm].Spawn();
+    }
+
+    private IEnumerator timer(float f, Action callback)
+    {
+        yield return new WaitForSeconds(f);
+
+        timeToWait -= decrementTimeToWait;
+        if (timeToWait < minTimetoWait)
+            timeToWait = minTimetoWait;
+
+        callback();
+
+        nextMeteo = StartCoroutine(timer(timeToWait, TriggerSpawner));
+    }
+}
