3852a2dfe6d6ba3114f89be6474ccc5a5450e313
Etienne Champagne <etienne.champagne.ec@gmail.com>
2019-01-26 12:31:26 +0100

    2 players

---
+++ GGJ_2019/Assets/Scripts/Wind.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] private Transform[] walls;
+    [SerializeField] private Vector2 direction;
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		foreach(Transform wall in walls)
+        {
+        }
+	}
+}
