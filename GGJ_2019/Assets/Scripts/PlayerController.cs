b9c800d0e088ef603f1dd2221dd2fb2482acbbfe
Vincent YVERT <v.yvert@student.isartdigital.com>
2019-01-26 17:17:51 +0100

    LA MANAITE

--- GGJ_2019/Assets/Scripts/PlayerController.cs
+++ GGJ_2019/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Controls
+enum Player
 {
-    Arrow,
-    ZQSD
+    Player1,
+    Player2
 }
 
 public class PlayerController : MonoBehaviour
@@ -13,10 +13,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float Speed;
     private Rigidbody _rigidBody;
 
-    [SerializeField] private Controls controls;
+    [SerializeField] private Player m_PlayerNumber;
     private List<Item> items;
 
     public bool IsActive;
+
+    private Vector2 m_Velocity;
 
     private void Awake()
     {
@@ -46,72 +48,30 @@ public class PlayerController : MonoBehaviour
         GameManager.Instance.PlayerLost(this);
     }
 
+    private void Update()
+    {
+        m_Velocity = new Vector2();
+
+        if (m_PlayerNumber == Player.Player1)
+        {
+            m_Velocity.x = Input.GetAxisRaw("Horizontal1");
+            m_Velocity.y = Input.GetAxisRaw("Vertical1");
+        }
+        else
+        {
+            m_Velocity.x = Input.GetAxisRaw("Horizontal2");
+            m_Velocity.y = Input.GetAxisRaw("Vertical2");
+        }
+
+        m_Velocity.Normalize();
+    }
+
     private void FixedUpdate()
     {
         if (!IsActive)
             return;
 
-        Vector2 velocity = new Vector2();
-        if (controls == Controls.ZQSD)
-        {
-            if (Input.GetKey(KeyCode.Z))
-            {
-                velocity.y += 1;
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                velocity.y -= 1;
-            }
-            else
-            {
-                velocity.y = 0;
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                velocity.x += 1;
-            }
-            else if (Input.GetKey(KeyCode.Q))
-            {
-                velocity.x -= 1;
-            }
-            else
-            {
-                velocity.x = 0;
-            }
-        }
-        else if (controls == Controls.Arrow)
-        {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                velocity.y += 1;
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                velocity.y -= 1;
-            }
-            else
-            {
-                velocity.y = 0;
-            }
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                velocity.x += 1;
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                velocity.x -= 1;
-            }
-            else
-            {
-                velocity.x = 0;
-            }
-        }
-
-        velocity.Normalize();
-
-        Vector3 movement = new Vector3(velocity.x, 0, velocity.y) * Speed * Time.deltaTime;
+        Vector3 movement = new Vector3(m_Velocity.x, 0, m_Velocity.y) * Speed * Time.deltaTime;
 
         _rigidBody.velocity = Vector3.zero;
         _rigidBody.AddForce(movement);
