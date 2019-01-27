b5f233462034f9ca50767c094556e578e29b3b67
Vincent YVERT <v.yvert@student.isartdigital.com>
2019-01-27 13:14:59 +0100

    LE SON DU DEBUT

--- GGJ_2019/Assets/Scripts/MainMenu.cs
+++ GGJ_2019/Assets/Scripts/MainMenu.cs
@@ -7,11 +7,16 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField]
     private GameMusic m_GameMusic;
-	
-	void Update ()
+
+    [SerializeField]
+    private GameObject m_LaunchSFX;
+
+    void Update ()
     {
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0))
         {
+            Instantiate(m_LaunchSFX);
+
             m_GameMusic.SwitchToGameMusic();
 
             SceneManager.LoadScene("LD1Launch");
