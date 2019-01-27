06d5edb0234eabcbdb56b670afdb89da06288ebe
Vincent YVERT <v.yvert@student.isartdigital.com>
2019-01-27 01:11:41 +0100

    TONNERRE DE BREST

--- GGJ_2019/Assets/Scripts/StormSounds.cs
+++ GGJ_2019/Assets/Scripts/StormSounds.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class StormSounds : MonoBehaviour
 {
     [SerializeField] private GameObject m_LigthSwitchSFXPrefab;
+    [SerializeField] private GameObject m_ThunderSFXPrefab;
 
     [SerializeField] private List<AudioClip> m_LightSwitchAudioClips;
+    [SerializeField] private List<AudioClip> m_ThunderAudioClips;
 
-	public void PlayLightSwitchSound()
+    public void PlayLightSwitchSound()
     {
         if (m_LigthSwitchSFXPrefab)
         {
             GameObject sfx = Instantiate(m_LigthSwitchSFXPrefab);
             sfx.GetComponent<SFX>().PlaySound(m_LightSwitchAudioClips[Random.Range(0, m_LightSwitchAudioClips.Count)]);
+        }
+    }
+
+    public void PlayThunderSound()
+    {
+        if (m_ThunderSFXPrefab)
+        {
+            GameObject sfx = Instantiate(m_ThunderSFXPrefab);
+            sfx.GetComponent<SFX>().PlaySound(m_ThunderAudioClips[Random.Range(0, m_ThunderAudioClips.Count)]);
         }
     }
 }
