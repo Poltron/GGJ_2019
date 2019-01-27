0de39c296ae4221fd464f0151438782d53f9e004
Vincent YVERT <v.yvert@student.isartdigital.com>
2019-01-27 14:20:29 +0100

    gbh'tz

--- GGJ_2019/Assets/Scripts/GameMusic.cs
+++ GGJ_2019/Assets/Scripts/GameMusic.cs
@@ -21,16 +21,16 @@ public class GameMusic : MonoBehaviour
 
     private IEnumerator FadeToGame()
     {
-        StartCoroutine(FadeOut());
+        StartCoroutine(FadeOutMenu());
 
         yield return new WaitForSeconds(m_FadeDuration / 3);
 
         m_GameAudioSource.Play();
 
-        StartCoroutine(FadeIn());
+        StartCoroutine(FadeInGame());
     }
 
-    private IEnumerator FadeOut()
+    private IEnumerator FadeOutMenu()
     {
         for (float timer = 0; timer < m_FadeDuration; timer += Time.deltaTime)
         {
@@ -42,7 +42,7 @@ public class GameMusic : MonoBehaviour
         m_MenuAudioSource.volume = 0;
     }
 
-    private IEnumerator FadeIn()
+    private IEnumerator FadeInGame()
     {
         for (float timer = 0; timer < m_FadeDuration; timer += Time.deltaTime)
         {
@@ -52,5 +52,45 @@ public class GameMusic : MonoBehaviour
         }
 
         m_GameAudioSource.volume = 0.9f;
+    }
+
+    public void SwitchToMenuMusic()
+    {
+        StartCoroutine("FadeToMenu");
+    }
+
+    private IEnumerator FadeToMenu()
+    {
+        StartCoroutine(FadeOutGame());
+
+        yield return new WaitForSeconds(m_FadeDuration / 3);
+
+        m_GameAudioSource.Play();
+
+        StartCoroutine(FadeInMenu());
+    }
+
+    private IEnumerator FadeOutGame()
+    {
+        for (float timer = 0; timer < m_FadeDuration; timer += Time.deltaTime)
+        {
+            m_GameAudioSource.volume = Mathf.Lerp(0.9f, 0, timer / m_FadeDuration);
+
+            yield return null;
+        }
+
+        m_GameAudioSource.volume = 0.9f;
+    }
+
+    private IEnumerator FadeInMenu()
+    {
+        for (float timer = 0; timer < m_FadeDuration; timer += Time.deltaTime)
+        {
+            m_MenuAudioSource.volume = Mathf.Lerp(0, 1, timer / m_FadeDuration);
+
+            yield return null;
+        }
+
+        m_MenuAudioSource.volume = 1;
     }
 }
