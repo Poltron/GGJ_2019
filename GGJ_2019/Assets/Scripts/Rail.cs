e0dc4115b41080387cfc5dc118f6ae4ebf98f060
Etienne Champagne <etienne.champagne.ec@gmail.com>
2019-01-27 02:21:36 +0100

    rail fix

--- GGJ_2019/Assets/Scripts/Rail.cs
+++ GGJ_2019/Assets/Scripts/Rail.cs
@@ -8,25 +8,31 @@ public class Rail : MonoBehaviour
     [SerializeField] private Transform Top;
     [SerializeField] private Transform Wall;
 
+    private Vector3 initialPosition;
+
+    private void Awake()
+    {
+        initialPosition = Wall.localPosition;
+    }
+
     void Start()
     {
-		
 	}
 	
 	void Update()
     {
-        Wall.localPosition = new Vector3(0, 0, Wall.localPosition.z);
+        Wall.localPosition = new Vector3(initialPosition.x, initialPosition.y, Wall.localPosition.z);
     }
 
     private void LateUpdate()
     {
         if (Wall.localPosition.z > Top.localPosition.z)
         {
-            Wall.localPosition = new Vector3(0, 0, Top.localPosition.z);
+            Wall.localPosition = new Vector3(initialPosition.x, initialPosition.y, Top.localPosition.z);
         }
         else if (Wall.localPosition.z < Bot.localPosition.z)
         {
-            Wall.localPosition = new Vector3(0, 0, Bot.localPosition.z);
+            Wall.localPosition = new Vector3(initialPosition.x, initialPosition.y, Bot.localPosition.z);
         }
     }
 }
